0b7592a3c7fba26b2cc581cc6ab0e13888835a58
Miguel Carvajal <omar@carvajalonline.com>
2025-04-24 01:54:43 -0400

    feat: added Paraglide JS for i18n

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -1,7 +1,19 @@
 // app.config.ts
 import { defineConfig } from "@tanstack/react-start/config";
 
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+
 export default defineConfig({
+  vite: {
+    plugins: [
+      paraglideVitePlugin({
+        project: "./project.inlang",
+        outdir: "./app/paraglide",
+        // We get the locale in this order: override from cookie, browser setting, default (usually English)
+        strategy: ["cookie", "preferredLanguage", "baseLocale"],
+      }),
+    ],
+  },
   server: {
     experimental: {
       websocket: true,
